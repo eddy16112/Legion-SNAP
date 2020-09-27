bd9b123d58b28abd7b6005f41ffa816a6f41871d
Wei Wu <eddy16112@gmail.com>
2020-09-27 04:12:23 -0600

    hipcuda

--- src/gpu_sweep.cu
+++ src/gpu_sweep.cu
@@ -15,7 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+extern "C" {
+cudaStream_t hipGetTaskStream();
+}
 #include <cstdio>
 
 #include "snap.h"
